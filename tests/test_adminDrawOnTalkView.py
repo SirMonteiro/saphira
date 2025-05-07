96b47ce399126efda239ff49f0fb28a4e0f5335a
Caio Bernardo <98186360+caio-bernardo@users.noreply.github.com>
2025-05-06 21:52:40 -0300

    Chore: Testar adminDrawOnTalkView (#52)
    * build: add djangorestframework typing as a dependecy
    
    * chore(api/views.py): add assertion to random_presence
    - It should never fail, but if ever did it will crash
    - Also for shut up editor warnings
    
    * feat(tests): add tests to adminDrawOnTalkView
    
    * Update workflow django.yml
    
    pasta src/testes mudou de local para tests/

---
+++ tests/test_adminDrawOnTalkView.py
@@ -0,0 +1,74 @@
+import datetime
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.test import APIClient, APITestCase, force_authenticate
+
+from api.models import Presence, Student, Talk
+
+class AdminDrawOnTalkTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.admin = User.objects.create_superuser(
+            username="test_admin",
+            email="foo@example.com",
+            password="1234"
+        )
+
+
+    def test_invalid_talk(self):
+        """Testa o endpoint com um id de palestra falso"""
+        fake_id = 404
+        self.client.force_login(user=self.admin)
+        response: Response = self.client.get(self.url(fake_id), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["error"], f"Palestra com id {fake_id} não encontrada.")
+
+
+    def test_valid_talk(self):
+        """Testa o endpoint com uma palestra valida mas sem presenças"""
+        talk = Talk.objects.create(
+            title="Palestra do Neymar",
+            speaker="Neymar",
+            description="A palestra do neymar",
+            date_time=datetime.datetime.now()
+        )
+
+        self.client.force_login(user=self.admin)
+        response: Response = self.client.get(self.url(talk.id), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["error"], "Nenhum estudante presente nesta palestra.")
+
+
+    def test_get_student_from_talk(self):
+        """Testa endpoint para retornar um estudante"""
+
+        talk = Talk.objects.create(
+            title="Palestra do Neymar",
+            speaker="Neymar",
+            description="A palestra do neymar",
+            date_time=datetime.datetime.now()
+        )
+
+        student = Student.objects.create(
+            name = "Glauber",
+            email = "glauber@email.com",
+        )
+
+        presence = Presence.objects.create(
+            student=student,
+            talk=talk,
+            online=False
+        )
+
+        self.client.force_login(user=self.admin)
+        response: Response = self.client.get(self.url(talk.id), format="json")
+
+
+    # Retorna a url com o talk_id
+    def url(self, id: int) -> str:
+        return reverse("admin-draw-on-talk", args=[id])
